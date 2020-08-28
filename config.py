a7afff39771266cc79bfecdd7c5efcffd4be0882
BenS123-discord <70386334+BenS123-discord@users.noreply.github.com>
2020-08-28 17:32:27 +0100

    Add files via upload

---
+++ config.py
@@ -0,0 +1,101 @@
+# Bot's token
+token = "NzQ4NTA4ODQ5MjMyODA1OTEw.X0edMA.YLyyseOp6LJWEh1CG2LoDZcNupA"
+
+# DiscordBotList token
+dbl_token = ""
+
+# Discord Bots token
+dbots_token = ""
+
+# Bots on Discord token
+bod_token = ""
+
+# Bots for Discord token
+bfd_token = ""
+
+# Discord Boats token
+dboats_token = ""
+
+# Sentry URL
+sentry_url = ""
+
+# Whether the bot is for testing, if true, stats and errors will not be posted
+testing = True
+
+# PUBSUB channel for Redis
+ipc_channel = ""
+
+# Postgres database credentials
+database = {
+    "database": "",
+    "user": "",
+    "password": "",
+    "host": "",
+    "port": 5432,
+}
+
+# Number of shards for each cluster
+shards_per_cluster = 4
+
+# Additional shards to launch
+additional_shards = 0
+
+# The default prefix for commands
+default_prefix = "="
+
+# The server to send tickets to, no confirmation messages if set
+default_server = None
+
+# Status of the bot
+activity = f"DM to Contact TP10 Staff"
+
+# Whether or not to fetch all members
+fetch_all_members = True
+
+# The main bot owner
+owner = 397101726047666197
+
+# Bot owners that have access to owner commands
+owners = []
+
+# Bot admins that have access to admin commands
+admins = []
+
+# Cogs to load on startup
+initial_extensions = [
+    "cogs.admin",
+    "cogs.communication",
+    "cogs.configuration",
+    "cogs.core",
+    "cogs.direct_message",
+    "cogs.error_handler",
+    "cogs.events",
+    "cogs.general",
+    "cogs.miscellaneous",
+    "cogs.modmail_channel",
+    "cogs.owner",
+    "cogs.premium",
+    "cogs.snippet",
+]
+
+# Channels to send logs
+join_channel = 000000000000000000
+event_channel = 000000000000000000
+admin_channel = 000000000000000000
+
+# This is where patron roles are at
+main_server = 000000000000000000
+
+# Patron roles for premium servers
+premium1 = 000000000000000000
+premium3 = 000000000000000000
+premium5 = 000000000000000000
+
+# The colour used in embeds
+primary_colour = 0x1E90FF
+user_colour = 0x00FF00
+mod_colour = 0xFF4500
+error_colour = 0xFF0000
+
+# Version of bot
+__version__ = "2.0.0"
